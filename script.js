676c1be4eba66ce6f932e710b714fe6a5895bb32
jec15 <vermin151983@gmail.com>
2022-08-13 01:22:22 -0300

    Add function that simulates the computer's choice

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-
+function getComputerChoice() {
+  // Generate random number between one and three
+  let randNum = Math.floor(Math.random() * 3) + 1;
+  
+  return (randNum === 1) ? 'rock' :
+    (randNum === 2) ? 'paper' :
+    'scissors';
+}
 
