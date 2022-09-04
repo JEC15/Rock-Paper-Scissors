769004ee908267f6ae6dd096f1423a04dc1c2e6f
jec15 <vermin151983@gmail.com>
2022-09-04 19:33:07 -0300

    Add game UI

--- script.js
+++ script.js
@@ -1,160 +1,91 @@
 function getComputerChoice() {
-  // Generate random number between one and three
-  let randNum = Math.floor(Math.random() * 3) + 1;
-  
+  let randNum = Math.floor(Math.random() * 3);
   return (randNum === 1) ? 'rock' :
     (randNum === 2) ? 'paper' :
     'scissors';
 }
 
-function getPlayerChoice(round, maxRounds) {
-  // Initialize our choice variable as an empty string
-  let choice = '';
-  // Loop until player enters a valid input
-  while (!choice) {
-    // Get player input
-    choice = getStringFromInput(`Round ${round} of ${maxRounds}
-Choose Rock, Paper or Scissors`);
-    
-    // If input is a valid choice, or player presses "cancel" on prompt
-    if (choice === 'rock' || choice === 'paper' || 
-      choice === 'scissors' || choice === 'cancelled by player') {
-      return choice;
-    }
-    // If input is an empty string, keep asking for valid input
-    if (!choice) {
-      alert('You must choose one of Rock, Paper or Scissors!');
-    } 
-    // If input is not what we're looking for, keep asking for valid input
-    else {
-      alert('Please choose only one of Rock, Paper or Scissors');
-      // Set choice to empty string so we don't exit the loop
-      choice = '';
-    }
-  }
+function capitalizeFirstLetter(word) {
+  return `${word[0].toUpperCase()}${word.slice(1)}`;
 }
 
-function playRound(playerSelection, computerSelection) {
-  // If player pressed "cancel" on prompt
-  if (playerSelection === 'cancelled by player') {
-    return playerSelection;
-  }
-  // Capitalize first letter of each string
-  playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
-  computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1);
-  // We can just compare strings here to get a winner
-  return playerSelection < computerSelection ? 
-    `You win! ${playerSelection} beats ${computerSelection}!` :
-  playerSelection === computerSelection ?
-    `Draw!` :
-    `You lose! ${computerSelection} beats ${playerSelection}`;
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  // Player chooses number of rounds to play
-  let maxRounds = getMaxRounds();
-  // Player cancelled game
-  if (maxRounds === 'cancelled by player') {
-    return;
-  }
-  // Loop as many times as maxRounds, calling the playRound function each time
-  for (i = 1; i <= maxRounds; i++) {
-    let currentRound = playRound(getPlayerChoice(i, maxRounds), getComputerChoice());
-    // Player cancelled game
-    if (currentRound === 'cancelled by player') {
-      return;
-    }
-    alert(`Round ${i} of ${maxRounds}
-${currentRound}`);
-    // Increase player scores, or don't do anything if draw
-    if (currentRound.startsWith('You win')) {
-      playerScore++;
-    } else if (currentRound.startsWith('You lose')) {
-      computerScore++;
-    }
-  }
-  // Print game results
-  if (playerScore > computerScore) {
-    alert(`You win the game!\n\nYour score: ${playerScore}
-Computer score: ${computerScore}`);
-  } else if (computerScore > playerScore) {
-    alert(`You lose the game\n\nYour score: ${playerScore}
-Computer score: ${computerScore}`);
+function toggleVisibility(elem, displayStyle) {
+  if (elem.style.display === 'none') {
+    elem.style.display = `${displayStyle}`;
   } else {
-    alert(`Game is draw!\n\nYour score: ${playerScore}
-Computer score: ${computerScore}`);
+    elem.style.display = 'none';
   }
 }
 
-function getStringFromInput(promptMessage) {
-  let input = prompt(promptMessage);
-  // If player presses the cancel button on prompt
-  if (input === null) {
-    return 'cancelled by player';
-  }
-  return input.toLowerCase().trim();
-}
+function playRound(playerChoice) {  
+  const compChoice = getComputerChoice();
+  let result = '';
+  const choices = ['rock', 'paper', 'scissors'];
 
-function getNumberFromInput(promptMessage) {
-  let input = '';
-  // Loop until we get a valid input
-  while (!input) {
-    input = prompt(promptMessage);
-    // If player pressed the cancel button on prompt
-    if (input === null) {
-      return 'cancelled by player';
-    }
-    // Try to convert input to number
-    input = parseInt(input);
-    // Check that input is a positive finite number
-    if (isFinite(input)) {
-      return input;
-    }
-    // If we didn't get a number from input, keep looping
-    alert('You must choose a number');
-    input = '';
+  if ((choices.indexOf(playerChoice) + 1) % 3 === choices.indexOf(compChoice)) {
+    compScore++;
+    result = `You lose. ${capitalizeFirstLetter(compChoice)} beats ${capitalizeFirstLetter(playerChoice)}.`;
+  } else if (choices.indexOf(playerChoice) === choices.indexOf(compChoice)) {
+    result = `Tie!`;
+  } else {
+    playerScore++;
+    result = `You win! ${capitalizeFirstLetter(playerChoice)} beats ${capitalizeFirstLetter(compChoice)}.`;
   }
-}
 
-function getMaxRounds() {
-  let maxRounds = 0;
-  // Limit rounds to at least 1 but no more than 20
-  while (maxRounds < 1 || maxRounds > 20) {
-    maxRounds = getNumberFromInput('How many rounds would you like to play? (min:1/max:20)');
-    if ((maxRounds > 0 && maxRounds < 21) || maxRounds === 'cancelled by player') {
-      return maxRounds;
-    }
-    alert('You must choose a number between 1 and 20!');
+  currentRound++;
+  roundNum.textContent = `Round ${currentRound}`;
+  roundResult.textContent = result;
+  playerScoreDiv.textContent = `Your score: ${playerScore}`;
+  compScoreDiv.textContent = `Computer Score: ${compScore}`;
+
+  const gameWinner = checkGameWinner();
+  if (gameWinner) {
+    startBtn.textContent = 'Play again';
+    toggleVisibility(startBtn, 'inline');
+    gameButtons.forEach(button => toggleVisibility(button, 'none'));
+    gameResult.textContent = gameWinner;
   }
 }
 
-function start() {
-  let keepPlaying = true;
-  // keep looping until player doesn't want to play anymore
-  while (keepPlaying) {
-    game();
-    let playAgain = '';
-    // Ask player if they want to play another game
-    while (!playAgain) {
-      playAgain = getStringFromInput('Would you like to keep playing?(y/n)');
-      // If player doesn't want to keep playing or if they press cancel on prompt
-      if (playAgain === 'cancelled by player' ||
-      playAgain === 'n' || playAgain === 'no') {
-          keepPlaying = false;
-          return;
-      }
-      // If player chooses to keep playing, we exit the loop(keepPlaying is still set to true)
-      if (playAgain === 'y' || playAgain === 'yes') {
-        continue;
-      } else {
-      // If player input is invalid, keep asking for input
-        alert('Please choose yes or no');
-        playAgain = '';
-      }
-    }
-  }
+function checkGameWinner() {
+  return (playerScore === 5) ? 'You won the game!' :
+  (compScore === 5) ? 'You lost the game.' :
+  undefined;
 }
 
-start();
+function resetCounters() {
+  compScore = 0;
+  playerScore = 0;
+  currentRound = 0;
+}
+
+function clearText() {
+  roundNum.textContent = '';
+  roundResult.textContent = '';
+  playerScoreDiv.textContent = '';
+  compScoreDiv.textContent = '';
+  gameResult.textContent = '';
+}
+
+const startBtn = document.querySelector('.start');
+const gameButtons = document.querySelectorAll('#game-button');
+const roundNum = document.querySelector('.round-num');
+const playerScoreDiv = document.querySelector('.player-score');
+const compScoreDiv = document.querySelector('.comp-score');
+const roundResult = document.querySelector('.round-result');
+const gameResult = document.querySelector('.game-result');
+
+let compScore = 0;
+let playerScore = 0;
+let currentRound = 0;
+
+startBtn.addEventListener('click', () => {
+  resetCounters();
+  clearText();
+  gameButtons.forEach(button => toggleVisibility(button, 'inline'));
+  toggleVisibility(startBtn, 'block');
+});
+
+gameButtons.forEach(button => button.addEventListener('click', () => {  
+    playRound(button.className);
+}));
